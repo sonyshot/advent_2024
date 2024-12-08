b95db4aa3fcbe05213726768e1ac8de0e5518c54
sonyshot <weitzgm@gmail.com>
2024-12-07 22:17:56 -0700

    compress

--- src/puzzle_5.rs
+++ src/puzzle_5.rs
@@ -33,32 +33,17 @@ fn one(file_lines: Vec<&str>) -> u32 {
         }
     }
 
-    let sorter = build_sorter_bool(orderings);
+    let sorter = build_sorter_ord(orderings);
 
     updates
         .iter()
         .filter(|update| {
-            println!("checking {:?}", update);
-            update.windows(2).all(|x| sorter(x[0], x[1]))
-        }).map(|update| update[update.len()/2])
+            update
+                .windows(2)
+                .all(|x| sorter(x[0], x[1]) != Ordering::Greater)
+        })
+        .map(|update| update[update.len() / 2])
         .sum()
-}
-
-fn build_sorter_bool(rules: HashMap<u32, Vec<u32>>) -> impl Fn(u32, u32) -> bool {
-    move |a, b| {
-        if let Some(smallers) = rules.get(&a) {
-            if smallers.contains(&b) {
-                return true;
-            }
-        }
-        if let Some(smallers) = rules.get(&b) {
-            if smallers.contains(&a) {
-                return false;
-            }
-        }
-
-        return true;
-    }
 }
 
 fn two(file_lines: Vec<&str>) -> u32 {
@@ -87,14 +72,20 @@ fn two(file_lines: Vec<&str>) -> u32 {
         }
     }
 
-    let sorter_bool = build_sorter_bool(orderings.clone());
-    let sorter_ord = build_sorter_ord(orderings);
+    let sorter = build_sorter_ord(orderings);
 
     updates
         .iter_mut()
         .filter(|update| {
-            update.windows(2).any(|x| !sorter_bool(x[0], x[1]))
-        }).map(|update|{update.sort_by(|a, b| sorter_ord(*a, *b)); update}).map(|update| update[update.len()/2])
+            update
+                .windows(2)
+                .any(|x| sorter(x[0], x[1]) == Ordering::Greater)
+        })
+        .map(|update| {
+            update.sort_by(|a, b| sorter(*a, *b));
+            update
+        })
+        .map(|update| update[update.len() / 2])
         .sum()
 }
 
