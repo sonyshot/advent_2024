423fb712766e5f3f86eb7d10a797d61acf33c959
sonyshot <weitzgm@gmail.com>
2024-12-11 20:35:22 -0700

    finally

--- src/puzzle_8.rs
+++ src/puzzle_8.rs
@@ -1,11 +1,17 @@
-use std::{cmp::min, cmp::max, collections::{HashMap, HashSet}, fs::read_to_string, iter::zip};
-use itertools::iproduct;
+use itertools::{iproduct, Itertools};
+use std::{
+    cmp::max,
+    cmp::min,
+    collections::{HashMap, HashSet},
+    fs::read_to_string,
+    iter::zip,
+};
 
 use regex::Regex;
 
 pub fn solve() -> u32 {
     let filepath = "assets/puzzle_8/input";
-    one(read_to_string(filepath).unwrap().lines().collect())
+    two(read_to_string(filepath).unwrap().lines().collect())
 }
 
 fn one(file_lines: Vec<&str>) -> u32 {
@@ -14,8 +20,6 @@ fn one(file_lines: Vec<&str>) -> u32 {
 
     let cols = file_lines.len() as i32;
     let rows = file_lines[0].len() as i32;
-
-    println!("cols {} - rows {}", cols, rows);
 
     file_lines.iter().enumerate().for_each(|(i, &line)| {
         re.captures_iter(line).enumerate().for_each(|x| {
@@ -31,43 +35,78 @@ fn one(file_lines: Vec<&str>) -> u32 {
 
     let mut anodes: HashSet<(i32, i32)> = HashSet::new();
 
-    for (&antenna, positions) in antennas.iter() {
-        for (p1, p2) in iproduct!(positions, positions){
+    for (_, positions) in antennas.iter() {
+        for (p1, p2) in iproduct!(positions, positions) {
             if p1 == p2 {
                 continue;
             }
-            let sign = ((p1.1-p2.1)/(p1.0-p2.0)).signum();
-            println!("Antenna {} for {:?} and {:?}", antenna, p1, p2);
 
-            let dx = (p1.0-p2.0).abs();
-            let dy = (p1.1-p2.1).abs();
+            let dx = p2.0 - p1.0;
+            let dy = p2.1 - p1.1;
 
-            let anode_pos1 = (p1.0-dx, p1.1-dy);
-            let anode_pos2 = (p2.0+sign*dx, p2.1+sign*dy);
+            let anode_pos1 = (p1.0 - dx, p1.1 - dy);
+            let anode_pos2 = (p2.0 + dx, p2.1 + dy);
 
-            if 0 < anode_pos1.0 && anode_pos1.0 < cols && 0 < anode_pos1.1 && anode_pos1.1 < rows{
-                println!("inserting {:?}", anode_pos1);
+            if 0 <= anode_pos1.0 && anode_pos1.0 < cols && 0 <= anode_pos1.1 && anode_pos1.1 < rows
+            {
                 anodes.insert(anode_pos1);
             }
 
-            if 0 < anode_pos2.0 && anode_pos2.0 < cols && 0 < anode_pos2.1 && anode_pos2.1 < rows{
-                println!("inserting {:?}", anode_pos2);
+            if 0 <= anode_pos2.0 && anode_pos2.0 < cols && 0 <= anode_pos2.1 && anode_pos2.1 < rows
+            {
                 anodes.insert(anode_pos2);
             }
         }
     }
 
-    println!("{:?}", anodes);
-
     anodes.len() as u32
 }
 
 fn two(file_lines: Vec<&str>) -> u32 {
-    let re = Regex::new(r"don't\(\).*?do\(\)").unwrap();
+    let re = Regex::new(r"[^\.]").unwrap();
+    let mut antennas: HashMap<&str, Vec<(i32, i32)>> = HashMap::new();
 
-    let new_lines = re
-        .replace_all(file_lines.join("").as_str(), "")
-        .into_owned();
+    let cols = file_lines.len() as i32;
+    let rows = file_lines[0].len() as i32;
 
-    0
+    file_lines.iter().enumerate().for_each(|(i, &line)| {
+        re.captures_iter(line).enumerate().for_each(|x| {
+            let mat = x.1.get(0).unwrap();
+            let pos = (mat.start() as i32, i as i32);
+            if let Some(antenna) = antennas.get_mut(mat.as_str()) {
+                antenna.push(pos);
+            } else {
+                antennas.insert(mat.as_str(), vec![pos]);
+            }
+        });
+    });
+
+    let mut lines = HashSet::new();
+    let eps = 0.00001;
+    for (_, positions) in antennas.iter() {
+        
+        positions.iter().combinations(2).for_each(|combo| {
+            let p1 = combo[0];
+            let p2 = combo[1];
+            let dy = p2.1 - p1.1;
+            let dx = p2.0 - p1.0;
+
+            let m = dy as f32 / dx as f32;
+            let b = p2.1 as f32 - m*p2.0 as f32;
+
+            for i in 0..cols {
+                let y_val = m*(i as f32)+b;
+
+                if y_val < 0.0 || y_val >= rows as f32 {
+                    continue;
+                }
+
+                if (y_val.round() - y_val).abs() <= eps{
+                    lines.insert((i, y_val.round() as i32));
+                }
+            }
+        });
+    }
+
+    lines.len() as u32
 }
